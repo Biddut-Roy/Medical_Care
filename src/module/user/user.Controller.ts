ce8b1690d39bf3682baf004ac072e7b20b27bf9a
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-11-03 20:49:12 +0600

    try catch method user

--- src/module/user/user.Controller.ts
+++ src/module/user/user.Controller.ts
@@ -2,8 +2,20 @@ import { Request, Response } from "express";
 import { userServices } from "./user.services";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const Result = await userServices.createAdmin(req.body);
-  res.send(Result);
+  try {
+    const Result = await userServices.createAdmin(req.body);
+    res.status(200).json({
+      success: "true",
+      massage: " Admin create Successfully",
+      data: Result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: "false",
+      massage: error?.name || " Admin create Successfully",
+      data: error,
+    });
+  }
 };
 
 export const userController = { createAdmin };
