7611d2b717f19ad31da21c7cb5925c9b498a1fb0
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-11-19 22:09:09 +0600

    Cloudinary file all interfaces setup

--- src/App/module/user/user.router.ts
+++ src/App/module/user/user.router.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { userController } from "./user.Controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../../helpers/fileUploader";
 const router = express.Router();
 
 router.post(
   "/",
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
   userController.createAdmin
 );
 
--- src/helpers/fileUploader.ts
+++ src/helpers/fileUploader.ts
@@ -1,14 +1,14 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
-// import { v2 as cloudinary } from "cloudinary";
-// import { ICloudinaryResponse, IFile } from "../app/interfaces/file";
+import { v2 as cloudinary } from "cloudinary";
+import { ICloudinaryResponse, IFile } from "../App/interfaces/file";
 
-// cloudinary.config({
-//   cloud_name: "dbgrq28js",
-//   api_key: "173484379744282",
-//   api_secret: "eHKsVTxIOLl5oaO_BHxBQWAK3GA",
-// });
+cloudinary.config({
+  cloud_name: "dnn9rhn6e",
+  api_key: "883561578429992",
+  api_secret: "cUKk6BtS8Ilj0K-D4c3YA1dUReQ", // Click 'View API Keys' above to copy your API secret
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -21,25 +21,25 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// const uploadToCloudinary = async (
-//   file: IFile
-// ): Promise<ICloudinaryResponse | undefined> => {
-//   return new Promise((resolve, reject) => {
-//     cloudinary.uploader.upload(
-//       file.path,
-//       (error: Error, result: ICloudinaryResponse) => {
-//         fs.unlinkSync(file.path);
-//         if (error) {
-//           reject(error);
-//         } else {
-//           resolve(result);
-//         }
-//       }
-//     );
-//   });
-// };
+const uploadToCloudinary = async (
+  file: IFile
+): Promise<ICloudinaryResponse | undefined> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      file.path,
+      (error: Error, result: ICloudinaryResponse) => {
+        fs.unlinkSync(file.path);
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
 
 export const fileUploader = {
   upload,
-  //   uploadToCloudinary,
+  uploadToCloudinary,
 };
